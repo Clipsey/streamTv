c4da81f4f415c200fa5665fa941706507466eeb7
Clipsey <20288304+Clipsey@users.noreply.github.com>
2019-09-10 16:05:28 -0700

    Remove logger middleware

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,7 +5,7 @@ import rootReducer from '../reducers/root_reducer';
 import thunk from '../thunk/thunk';
 
 const configureStore = (preloadedState = {}) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+  return createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 }
 
 export default configureStore;
