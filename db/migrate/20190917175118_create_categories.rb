daddec8cb381208c158d64fcef27afd833c4042d
Clipsey <20288304+Clipsey@users.noreply.github.com>
2019-09-17 11:23:13 -0700

    test categories

--- db/migrate/20190917175118_create_categories.rb
+++ db/migrate/20190917175118_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :title
-      t.viewers :viewers
+      t.integer :viewers
       t.timestamps 
     end
   end
