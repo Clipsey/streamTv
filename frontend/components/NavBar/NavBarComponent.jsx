3611dc0e7a3939a004c09571abe82d1214bc9aca
Clipsey <20288304+Clipsey@users.noreply.github.com>
2019-10-14 10:37:36 -0700

    Remove Search bar

--- frontend/components/NavBar/NavBarComponent.jsx
+++ frontend/components/NavBar/NavBarComponent.jsx
@@ -197,24 +197,24 @@ export class NavBarComponent extends React.Component {
         </section>
         
         {/* Change text color to white in change handler */}
-        { showSearchBar && 
+        {/* { showSearchBar && 
           <input id="searchBar" style={searchInput} type="text"
             name="searchBar" onChange={this.handleChange('searchBar')}
             value={this.state.searchBar}
             placeholder="Search"
             className={searchClass} />
-        }
+        } */}
         {/* {showSearchBar && 
           <div style={searchIcon}> </div>
         } */}
 
         <section style={rightBarStyle}>
-          { !showSearchBar && 
+          {/* { !showSearchBar && 
             <div style={visualBoxRight}>Search</div>
-          }
+          } */}
           {!this.props.currentUser && <div style={visualBoxRight} className='buttonClass' onClick={this.toggleModal('login')}>Log In</div> }
           {!this.props.currentUser && <div style={visualBoxRight} className='buttonClass' onClick={this.toggleModal('signup')}>Sign Up</div> }
-          {this.props.currentUser && <div style={visualBoxRight} className='buttonClass' onClick={this.logoutCheck}>Sign Out</div> } 
+          {/* {this.props.currentUser && <div style={visualBoxRight} className='buttonClass' onClick={this.logoutCheck}>Sign Out</div> }  */}
           {this.props.currentUser && <div style={visualBoxRight} className='buttonClass' onClick={this.toggleUserDrop}>
             {this.props.currentUser.picture && <img style={profilePictureStyle} src={this.props.currentUser.picture}></img>}
             {!this.props.currentUser.picture && <svg id="svgProfile" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm7.753 18.305c-.261-.586-.789-.991-1.871-1.241-2.293-.529-4.428-.993-3.393-2.945 3.145-5.942.833-9.119-2.489-9.119-3.388 0-5.644 3.299-2.489 9.119 1.066 1.964-1.148 2.427-3.393 2.945-1.084.25-1.608.658-1.867 1.246-1.405-1.723-2.251-3.919-2.251-6.31 0-5.514 4.486-10 10-10s10 4.486 10 10c0 2.389-.845 4.583-2.247 6.305z" /></svg>}
